b3bf11329227486cfa847732af7fc267441efff0
Susan Ndagi <33904019+ndagisusan@users.noreply.github.com>
2022-12-05 14:49:29 +0300

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DevOps</h1>
+      <p>
+        The abbreviation "DevOps" refers to the replacement of segregated
+        Development and Operations. The goal is to form multidisciplinary teams
+        that collaborate using common, efficient procedures and technologies.
+        Agile planning, continuous integration, continuous delivery, and
+        application monitoring are all important DevOps strategies. DevOps is a
+        never-ending journey.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
